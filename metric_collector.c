967e84056390f51379ef8de376dc56cfccbe0e74
Sid Tirkey <sidhu.ku.tirkey@gmail.com>
2026-04-04 21:09:30 +0000

    Appended description

--- metric_collector.c
+++ metric_collector.c
@@ -35,7 +35,7 @@ int collect_cpu_metric(char *cpu, unsigned long *total_cpu_time, unsigned long *
 
 
 double cpu_util_interval(int sleep_time){
-	/*returns cpu utilisation in terms of a percentage */
+	/*returns cpu utilisation in terms of a percentage in double */
 	char cpu[10];
 	unsigned long total_cpu_util_time_1, idle_time_1, total_cpu_util_time_2, idle_time_2;
 	unsigned long delta_total_cpu_time, delta_idle;
