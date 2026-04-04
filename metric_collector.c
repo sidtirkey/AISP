daa7318a24b4387d8c5af61e35ae62d4d6eee696
Sid Tirkey <sidhu.ku.tirkey@gmail.com>
2026-04-04 20:54:04 +0000

    Added metric collector file

---
+++ metric_collector.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int collect_cpu_metric(char *cpu, unsigned long *total_cpu_time, unsigned long *idle_time){
+	FILE *fp = fopen("/proc/stat","r");
+	if (fp == NULL){
+		perror("Error opening /proc/stat\n");
+		exit(1);
+	}
+	unsigned long user, nice, system, idle, iowait, irq, softirq;
+	unsigned long steal, guest, guest_nice;
+	unsigned long sum_cpu_time;
+	int parse_result = fscanf(fp,"%s %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu", cpu, &user,
+			&nice, &system, &idle, &iowait, &irq, &softirq, &steal, &guest, &guest_nice);
+	if (parse_result == 11){
+		sum_cpu_time = user+nice+system+idle+iowait+irq+softirq+steal+guest+guest_nice;
+		*total_cpu_time = sum_cpu_time;
+		*idle_time = idle;
+	}
+	if (parse_result == 0){
+		perror("CPU metric collection failed because of parsing error\n");
+		exit(1);
+	}
+	if (parse_result == -1){
+		perror("Input failure, reached EOF before reaching any file\n");
+		exit(1);
+	}
+	fclose(fp);
+	return 0;
+}
+
+
+double cpu_util_interval(int sleep_time){
+	char cpu[10];
+	unsigned long total_cpu_util_time_1, idle_time_1, total_cpu_util_time_2, idle_time_2;
+	unsigned long delta_total_cpu_time, delta_idle;
+	double cpu_perc;
+	collect_cpu_metric(cpu, &total_cpu_util_time_1, &idle_time_1);
+	usleep(sleep_time * 1000);
+	collect_cpu_metric(cpu, &total_cpu_util_time_2, &idle_time_2);
+	delta_total_cpu_time = total_cpu_util_time_2 - total_cpu_util_time_1;
+	delta_idle = idle_time_2 - idle_time_1;
+	cpu_perc = (1.0 - (double)delta_idle/(double)delta_total_cpu_time)*100.0;
+	return cpu_perc;
+}
+
+
+int main(){
+	printf("%f", cpu_util_interval(100));
+	return 0;
+}
+
+
