14cfce09332c73f85509510a52e7afb37474698e
Sid Tirkey <sidhu.ku.tirkey@gmail.com>
2026-04-05 07:55:33 +0000

    Bug fixes related to return value

--- metric_collector.c
+++ metric_collector.c
@@ -35,6 +35,7 @@ int collect_cpu_metric(char *cpu, unsigned long *total_cpu_time, unsigned long *
 }
 
 int collect_memory_metric(unsigned long *mem_tot, unsigned long *mem_avail){
+	/*Parses /proc/meminfo and assigns MemTotal: and MemAvailable: field */
 	FILE *fp = fopen("/proc/meminfo", "r");
 	if (fp == NULL){
 		perror("Unable to open memory info file\n");
@@ -43,6 +44,8 @@ int collect_memory_metric(unsigned long *mem_tot, unsigned long *mem_avail){
 	char label[64];
 	char unit[10];
 	unsigned long value;
+	*mem_tot = 0;
+	*mem_avail = 0;
 	while (fscanf(fp, "%63s %lu %9s", label, &value, unit) == 3){
 		if (strcmp(label,"MemTotal:") == 0){
 			*mem_tot = value;
@@ -64,9 +67,15 @@ double cpu_util_interval(int sleep_time){
 	unsigned long delta_total_cpu_time, delta_idle;
 	double cpu_perc = 0.0;
 	int coll_cpu = collect_cpu_metric(cpu, &total_cpu_util_time_1, &idle_time_1);
+	if (coll_cpu == -1){
+		return -1;
+	}
 	usleep(sleep_time * 1000);
 	collect_cpu_metric(cpu, &total_cpu_util_time_2, &idle_time_2);
 	delta_total_cpu_time = total_cpu_util_time_2 - total_cpu_util_time_1;
+	if (delta_total_cpu_time == 0){
+		return 0.0;
+	}
 	delta_idle = idle_time_2 - idle_time_1;
 	cpu_perc = (1.0 - (double)delta_idle/(double)delta_total_cpu_time)*100.0;
 	return cpu_perc;
@@ -74,8 +83,14 @@ double cpu_util_interval(int sleep_time){
 
 
 double memory_util(){
+	/*returns used memory in KiloBytes */
 	unsigned long mem_tot, mem_avail;
+	mem_tot = 0;
+	mem_avail = 0;
 	int coll_mem = collect_memory_metric(&mem_tot, &mem_avail);
+	if (coll_mem == -1){
+		return -1;
+	}
 	double used_mem = mem_tot - mem_avail;
 	return used_mem;
 }
